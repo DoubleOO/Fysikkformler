59692b584df6fdea78cfa29453ec86c903a2a3a8
Coocooso <oscar.apeland@icloud.com>
2014-02-05 16:20:44 +0100

    Initial Commit

---
+++ Fysikkformler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fysikkformler
+//
+//  Created by Oscar Apeland on 05.02.14.
+//  Copyright (c) 2014 Oscar Apeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fysikkformler/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fysikkformler
+//
+//  Created by Oscar Apeland on 05.02.14.
+//  Copyright (c) 2014 Oscar Apeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
